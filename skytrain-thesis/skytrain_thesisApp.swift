086db82da586b5bb318d0e8562fbcfcd0c0319fd
Nantanat Thongthep <phat.nantanat@gmail.com>
2024-03-13 15:46:26 +0700

    Initial Commit

---
+++ skytrain-thesis/skytrain_thesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  skytrain_thesisApp.swift
+//  skytrain-thesis
+//
+//  Created by Nantanat Thongthep on 13/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct skytrain_thesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
